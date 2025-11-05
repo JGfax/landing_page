bccdf768700ad750b7b850975e5e98d1da9a8160
JGfax <joao.bicudo@aluno.senai.br>
2025-11-05 11:27:33 -0300

    update inserir

--- inserir.php
+++ inserir.php
@@ -27,16 +27,20 @@ $rows = $stmt->fetchAll();
   <h1>Inserir Relatório</h1>
   </div>
 <form method="post">
+
   <div class="conjunto">
   <label for=""><i class="fa-solid fa-user"></i></label>
   <input name="titulo" placeholder="Título" required><br>
   </div>
+
   <div class="conjunto">
   <label for=""> <i class="fa-solid fa-file-signature"></i></label>
     <input name="autor" placeholder="Autor"><br>
   </div>
+  
   <textarea name="descricao" placeholder="Descrição"></textarea><br>
   <button>Salvar</button>
+
 </form>
 </section>
 
