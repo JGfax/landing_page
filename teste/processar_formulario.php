8bdf7d940c9ebb2561636fe6be4f46f6bf73606b
JGfax <joao.bicudo@aluno.senai.br>
2025-11-11 08:53:27 -0300

    g

--- teste/processar_formulario.php
+++ teste/processar_formulario.php
@@ -4,25 +4,26 @@ require 'conexao.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $nome = $_POST['nome'];
     $email = $_POST['email'];
-    $respostas = $_POST['respostas'];
+    $telefone = $_POST['telefone'];
 
     try {
         $pdo->beginTransaction();
 
         // Insere usuário
-        $stmt = $pdo->prepare("INSERT INTO tbl_clientes (nome, email, telefone, id) VALUES (:nome, :email)");
-        $stmt->execute(['nome' => $nome, 'email' => $email]);
+        $stmt = $pdo->prepare("INSERT INTO tbl_servico (id_servico, descricao) VALUES (:id_servico, :descricao)");
+        $stmt->execute(['id_servico' => $id_servico, 'nome' => $nome['nome'], ' descricao' => $descricao['descricao']]);
         $id_usuario = $pdo->lastInsertId();
 
         // Insere respostas
         $stmt = $pdo->prepare("
-            INSERT INTO respostas (id_usuario, cor_favorita, animal_favorito)
-            VALUES (:id_usuario, :cor_favorita, :animal_favorito)
+            INSERT INTO tbl_cliente (id_cliente, nome, telefone, email)
+            VALUES (:id_cliente, :nome, :telefone, :email)
         ");
         $stmt->execute([
-            'id_usuario' => $id_usuario,
-            'cor_favorita' => $respostas['cor_favorita'],
-            'animal_favorito' => $respostas['animal_favorito']
+            'id_cliente' => $id_cliente,
+            'nome' => $nome['nome'],
+            'email' => $email['email'],
+            'telefone' => $telefone['telefone']
         ]);
 
         $pdo->commit();
