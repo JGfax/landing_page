0431682d8df1a5aca178c1b62b16f6731918c596
JGfax <joao.bicudo@aluno.senai.br>
2025-11-11 09:06:49 -0300

    atualização

---
+++ teste1/conexao.php
@@ -0,0 +1,17 @@
+<?php
+// db.php
+$host = 'localhost';
+$db   = 'landing';
+$user = 'root';
+$pass = 'Home@spSENAI2025!';
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+  PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+try {
+  $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+  die("DB connection failed: " . $e->getMessage());
+}
