6050c922161881334cdb7ec5161d2bd5bd449598
manuellavsilva6-sudo <manuella.v.silva6@aluno.senai.br>
2025-11-11 13:24:15 -0300

    atualização tela da tabela

--- testeprincipal/relatorio.php
+++ testeprincipal/relatorio.php
@@ -15,25 +15,52 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="relatorio.css">
     <link rel="stylesheet" href="inserir.php">
     <title>RELATÓRIOS</title>
+
 </head>
 <body>
-    
-<section>
-  <div class="section-header">
-  <h2>Relatórios gravados</h2>
 
+
+        
+  <div class="overlay"></div>
+
+  <div class="conteudo">
+
+  <!-- ================== MENU ================== -->
+    <section id="menu">
+        <nav class="menu">
+            <div class="logo"> 
+                <a href="index.html" target="_top">
+                </a>
+            </div>
+
+
+            <ul>
+                <li><a href="#">RELATÓRIOS</a></li>
+            </ul>
+        </nav>
+    
+<br><br><br><br><br><br>
+    <h1>Relatórios gravados</h1>
+  
+
+    
+<br><br><br><br><br>
+<section>
+    <p>Análise Detalhada de Resultados</p>
+
+  <div class="section-header">
+    
 <table border="1" cellpadding="6">
   <thead>
-  <tr><th style="width: 60%;">nome</th><th>email</th><th>telefone</th><th>servicos</th>
+  <tr><th style="width: 60%;">NOME</th><th>EMAIL</th><th>TELEFONE</th><th>SERVICOS</th>
   </thead>
   <tbody>
   <?php if (count($rows) === 0): ?>
     <tr>
-      <td colspan="5" style="text-align:center; padding:15px; color:#777;">
-        Não há registros.
-      </td>
+      <td colspan="5" style="text-align:center; padding:15px; color:#ffff;">Não há registros.</td>
     </tr>
   <?php else: ?>
     <?php foreach($rows as $r): ?>
@@ -48,8 +75,35 @@ try {
 </tbody>
 
 </table>  
+
+
 </section>
   </main>
+
+
+  <!-- ================== RODAPE ================== -->
+<br><br>
+  <section>
+        <footer>
+            <div class="footer-section">
+                <h3>Desenvolvido por:</h3>
+                <p>João Gustavo & Manuella Vitória</p>
+            </div>
+
+            <div class="footer-section">
+                <h3>Turma:</h3>
+                <p>1-A-SESI-TI - Desenvolvimento de Sistemas</p>
+            </div>
+        </footer>
+    </section>
+  </div>
+
+
+
+
+
+</body>
+</html>
 </body>
 </html>
 
